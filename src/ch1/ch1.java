80655ef4339d785e150210f956e78acf9e2a853e
SEOKHO <seokho9682@gmail.com>
2026-05-02 21:04:52 +0900

    2026.05.02 58p까지 학습

---
+++ src/ch1/ch1.java
@@ -0,0 +1,7 @@
+package ch1;
+
+public class ch1 {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
---
+++ src/ch2/ch2.java
@@ -0,0 +1,71 @@
+package ch2;
+
+public class ch2 {
+    public static void main(String[] args){
+        // 2-1
+        /*
+        int level;
+        level = 10;
+        System.out.println(level);*/
+
+        // 2-2
+        /*char ch1 = 'A';
+        System.out.println(ch1);
+        System.out.println((int)ch1);
+
+        char ch2 = 66;
+        System.out.println(ch2);
+
+        int ch3 = 67;
+        System.out.println(ch3);
+        System.out.println((char)ch3);*/
+
+        // 2-3
+        /*char ch1 = '한';
+        char ch2 = '\uD55C';
+
+        System.out.println(ch1);
+        System.out.println(ch2);*/
+
+        // 2-4
+        /*int a = 65;
+        int b = -65;
+
+        char a2 = 65;
+        System.out.println((char)a);
+        System.out.println((char)b);
+        System.out.println(a2);*/
+
+        // 2-5
+        /*double dnum = 3.14;
+        float fnum = 3.14F;
+
+        System.out.println(dnum);
+        System.out.println(fnum);*/
+
+        // 2-6
+        /*double dnum = 1;
+
+        for (int i = 0; i < 10000; i++){
+            dnum = dnum + 0.1;
+        }
+        System.out.println(dnum);*/
+
+        // 2-7
+        /*boolean isMarried = true;
+        System.out.println(isMarried);*/
+
+        // 2-8
+        var i = 10;
+        var j = 10.0;
+        var str = "hello";
+
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(str);
+
+        str = "test";
+
+
+    }
+}
