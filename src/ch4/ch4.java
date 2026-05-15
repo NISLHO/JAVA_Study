85138e8543a8ede192bcc7cef53f410904a34216
SEOKHO <seokho9682@gmail.com>
2026-05-15 18:22:49 +0900

    2026.05.15 120p까지 학습

--- src/ch4/ch4.java
+++ src/ch4/ch4.java
@@ -153,6 +153,37 @@ public class ch4 {
         System.out.println(value);*/
 
         // 되새김 문제2
+        /*for (int i = 2; i <= 9; i++) {
+            if (i % 2 != 0){
+                continue;
+            }
+            for (int j = 1; j <= 9; j++){
+                System.out.println(i + "X" + j + "=" + i*j);
 
+            }
+            System.out.println(" ");
+        }*/
+
+        // 되새김 문제 3
+        /*for (int i = 2; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++){
+                if (i < j){
+                    break;
+                }
+                System.out.println(i + "X" + j + "=" + i*j);
+            }
+            System.out.println(" ");
+        }*/
+        // 되새김 문제 4
+
+        for (int i = 1; i <= 4; i++){
+            for (int j = 4-i; j > 0 ; j--){
+                System.out.print(' ');
+            }
+            for (int k = 1; k <= 2*i-1;k++){
+                System.out.print('*');
+            }
+            System.out.println(' ');
+        }
     }
 }
